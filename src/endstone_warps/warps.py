d31f273834fc28ee14e3e408b88eef58bfe8ceab
rezn1r <rezn1r@proton.me>
2026-04-12 23:25:06 +0800

    fix: bump version to 1.0.1 in pyproject.toml and warps.py

--- src/endstone_warps/warps.py
+++ src/endstone_warps/warps.py
@@ -13,7 +13,7 @@ from typing_extensions import override
 class Warps(Plugin):
     
     description = "A lightweight warp plugin for Endstone."
-    version = "1.0.0"
+    version = "1.0.1"
     api_version = "0.11"
     authors = ["Rezn1r"]
 
