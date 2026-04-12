0c66c9880032cbfad88571ad8acf7f1a0d44a253
rezn1r <rezn1r@proton.me>
2026-04-12 21:21:41 +0800

    feat: made plugin shell, nothing functional yet

---
+++ src/endstone_warps/warps.py
@@ -0,0 +1,7 @@
+from endstone.plugin import Plugin
+
+class Warps(Plugin):
+    description = "A lightweight warp plugin for Endstone."
+    version = "1.0.0"
+    api_version = "0.11"
+    authors = ["Rezn1r"]
